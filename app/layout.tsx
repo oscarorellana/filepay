2d78ab3dd9395b91151d68fb4c1b0b3ed9f3fdad
Oscar <oscar_orellana@live.com>
2026-01-22 20:01:13 -0500

    add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: {
